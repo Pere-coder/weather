7dce89dc6c6a460888eb053f7aaf5d8f71fdb0b3
Pere <gpere800@gmail.com>
2023-06-01 19:38:13 -0700

    modified:   components/Weather.js 	modified:   next.config.js

--- components/Weather.js
+++ components/Weather.js
@@ -10,7 +10,9 @@ const Weather = ({data}) => {
         <div>
             <div>
                 <Image 
-                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt='/'/>
+                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt='/'
+                width='100'
+                height='100'/>
             </div>
         </div>
     </div>
