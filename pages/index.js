e8e864bb8e42bd8a268f1cec41e0c1d7a4ef97a1
Pere <gpere800@gmail.com>
2023-06-01 19:36:13 -0700

    new file:   components/Spinner.js 	new file:   components/Weather.js 	modified:   pages/index.js 	new file:   public/spinner.gif

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import axios from 'axios'
 import { useState } from 'react'
-import {BySearch} from 'react-icons/bs'
+import {BsSearch, BySearch} from 'react-icons/bs'
+import Weather from '@/components/Weather'
+import Spinner from '@/components/Spinner'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,30 +18,60 @@ export default function Home() {
 
 
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=brazil&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
   
   const fetchWeather = (e) => {
     e.preventDefault();
     setLoading(true);
     axios.get(url).then((response) => {
       setWeather(response.data);
-      console.log(response.data);
+      // console.log(response.data);
     });
     setCity('');
     setLoading(false);
   };
+
+
+  if (loading) {
+    return <Spinner/>
+  }
+  else {
+    return (
+      <div>
   
-  return (
-    <div>
-
-    <div className='absolute top-0 left-0 right-0 bottom-0 bg-black z-[1'/>
-      <Image src='https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=904&q=80'
-       layout='fill'
-       className='object-cover'/>
-
-
-
-    </div>
-
-  )
-}
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'/>
+        <Image src='https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=904&q=80'
+         layout='fill'
+         className='object-cover'/>
+  
+  
+  
+         {/* Search */}
+         <div className='mt-10 relative flex justify-between items-center max-w-[500px] w-full m-auto text-white z-10'>
+          <form onSubmit={fetchWeather} 
+           className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'>
+            <div>
+              <input onChange={(e) => setCity(e.target.value)}
+               className='bg-transparent border-none text-white focus:outline-none text-2xl' type="text" placeholder='Search city'/>
+            </div>
+            <button onClick={fetchWeather}><BsSearch size={20}/></button>
+          </form>
+         </div>
+  
+  
+         {/* weather */}
+  
+         {weather.main && <Weather data={weather}/>}
+  
+  
+  
+  
+  
+      </div>
+  
+    )
+  }
+  
+  }
+  
+  
